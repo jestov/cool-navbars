a9f96f3b7d11ba7fdb941e102a78f6ed9aa8a2a0
JesusT97 <elbauldelocio@gmail.com>
2018-12-28 17:49:15 -0600

    UX for main nav

--- js/tabs.js
+++ js/tabs.js
@@ -1,7 +1,9 @@
 const indicators = document.querySelectorAll("nav#main-nav ul li a");
+const container = document.querySelector("details");
 for(var i = 0, l = indicators.length; i < l; i++) {
     var indicator = indicators[i];
     indicator.addEventListener("click", function(i) {
+        container.open = false;
         var targets = document.querySelectorAll("[data-tab]");
         for(var j = 0, targetsLength = targets.length; j < targetsLength; j++) {
             var target = targets[j];
