3de6701763ce5289770a6ab3b05701a407bc7433
JesusT97 <elbauldelocio@gmail.com>
2018-12-30 01:26:36 -0600

    fix tabs

--- js/tabs.js
+++ js/tabs.js
@@ -1,21 +1,17 @@
 const indicators = document.querySelectorAll("nav#main-nav ul li a");
 const container = document.querySelector("details");
-for(var i = 0, l = indicators.length; i < l; i++) {
-    var indicator = indicators[i];
-    indicator.addEventListener("click", function(i) {
-        container.open = false;
+    indicators.forEach(el => el.addEventListener("click", function(){
+        indicators.forEach(el => el.classList.remove("active"));
         var targets = document.querySelectorAll("[data-tab]");
         for(var j = 0, targetsLength = targets.length; j < targetsLength; j++) {
             var target = targets[j];
-            if(this.getAttribute("href") == target.getAttribute("data-tab")) {
-                console.log("entra");
+            if(el.getAttribute("href") == target.getAttribute("data-tab")) {
                 target.classList.add("active");
-                this.classList.add("active");
+                el.classList.add("active");
             }
             else {
                 target.classList.remove("active");
             }
         }
-        indicator.classList.remove("active");
-    });
-}
+        container.open = false;
+    }));
